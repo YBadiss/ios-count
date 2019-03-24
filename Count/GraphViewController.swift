535c452d2123641ff76b382f65064e81772ceaab
Yacine Badiss <yacine.badiss@gmail.com>
2019-03-24 16:46:55 +0100

    Add dummy graph

--- Count/GraphViewController.swift
+++ Count/GraphViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import Charts
 
 class GraphViewController: UIViewController {
+    @IBOutlet weak var chartView: LineChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        var lineChartEntry = [ChartDataEntry]()
+        for i in 0...10 {
+            lineChartEntry.append(ChartDataEntry(x: Double(i), y: Double(i*i)))
+        }
+        let line1 = LineChartDataSet(values: lineChartEntry, label: "Number")
+        line1.colors = [#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)]
+        
+        let data = LineChartData()
+        data.addDataSet(line1)
+        chartView.data = data
+        chartView.chartDescription?.text = "My awesome chart"
     }
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
--- Count/UIViewExtension.swift
+++ Count/UIViewExtension.swift
@@ -3,13 +3,13 @@ import UIKit
 extension UIView {
     var firstResponder: UIView? {
         guard !isFirstResponder else { return self }
-        
+
         for subview in subviews {
             if let firstResponder = subview.firstResponder {
                 return firstResponder
             }
         }
-        
+
         return nil
     }
 }
